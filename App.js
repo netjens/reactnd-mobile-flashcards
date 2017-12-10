c73eecf207b8077513c4c4f91499a4fb1e877516
netjens <mailtojens@gmail.com>
2017-12-10 17:31:36 +0100

    feat: first version of home page

--- App.js
+++ App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import { purple, white } from './utils/colors'
+import { Constants } from 'expo'
+import {StackNavigator } from 'react-navigation'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Provider store={createStore(reducer)}>
+      <View style={{flex: 1}}>
+        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+        <MainNavigator />
       </View>
+    </Provider>
     );
   }
+}
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  EntryDetail: {
+    screen: EntryDetail,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  }
+})
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
