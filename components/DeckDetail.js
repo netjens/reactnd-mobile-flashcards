b0f2884f8c67d4577f6285e4036134e162b1a9fd
netjens <mailtojens@gmail.com>
2017-12-25 23:16:48 +0100

    feat: add buttons to DeckDetail view

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,51 +1,102 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { white,purple } from '../utils/colors'
 
 
-class DeckDetail extends Component{
 
-    render(){
+class DeckDetail extends Component {
+
+
+
+    addCard = () => {
+
+
+    }
+
+
+    render() {
         const deck = this.props.deck;
         return (
             <View style={{
                 backgroundColor: '#f4f4f4', flex: 1,
                 flexDirection: 'column',
                 justifyContent: 'flex-start'
-              }}>
+            }}>
 
-                <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>{deck.title}</Text>
-                <Text style={{ textAlign: 'center' }}>{deck.questions.length} cards</Text>
+                <Text style={styles.title}>{deck.title}</Text>
+                <Text style={styles.subTitle}>{deck.questions.length} cards</Text>
+                <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}> 
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={this.addCard}>
+                        <Text style={styles.buttonText}>Add Card</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={this.startQuiz}>
+                        <Text style={styles.buttonText}>Start Quiz</Text>
+                    </TouchableOpacity>
 
+                </View>
             </View>
-
-
-
         )
-
-
     }
 
 }
 
-function mapStateToProps (state, { navigation }) {
+
+const styles = StyleSheet.create({
+    title: {
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 20,
+        textAlign: 'center',
+        fontSize: 40
+    },
+    subTitle: {
+        fontSize: 20,
+        color: 'gray',
+        textAlign: 'center'
+    },
+    button: {
+
+        backgroundColor: purple,
+        padding: 10,
+        marginBottom: 30,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 5,
+    },  
+    buttonText: {
+        color: white,
+        fontSize: 22,
+        textAlign: 'center',
+      }
+}
+);
+
+
+function mapStateToProps(state, { navigation }) {
     const { deckKey } = navigation.state.params;
     const deck = state[deckKey];
     console.log("in stateToProps=" + JSON.stringify(deck));
     return (
-      {deck: deck});
-    
-  }
-  
-  function mapDispatchToProps (dispatch, { navigation }) {
+        { deck: deck });
+
+}
+
+function mapDispatchToProps(dispatch, { navigation }) {
     const { deckKey } = navigation.state.params
-  
+
     return {
-      goBack: () => navigation.goBack(),
+        goBack: () => navigation.goBack(),
     }
-  }
-  
-  export default connect(
+}
+
+export default connect(
     mapStateToProps,
     mapDispatchToProps,
-  )(DeckDetail)
+)(DeckDetail)
