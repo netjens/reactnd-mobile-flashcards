4ed6521e98ed91201adf71a9582471d6764750d0
netjens <mailtojens@gmail.com>
2017-12-28 13:49:52 +0100

    feat: first try of flip implementation

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,43 +1,82 @@
 import React, { Component } from 'react'
+import FlipView from 'react-native-flip-view-next'
 import {
     View,
     Text,
     TextInput,
     TouchableOpacity,
     Platform,
-    StyleSheet
+    StyleSheet,
+    Easing
 } from 'react-native'
 import { purple, white } from '../utils/colors'
 import { saveDeckTitle } from '../utils/api'
 import { NavigationActions } from 'react-navigation'
 import { connect } from 'react-redux'
+import TextButton from './TextButton'
 
 class Quiz extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            currentIndex: 0
+            currentIndex: 0,
+            isCardFlipped: false
         };
     }
 
 
+    flip = () => {
+        this.setState({ isFlipped: !this.state.isFlipped });
+    }
+
     render() {
         const deck = this.props.deck;
-        const {currentIndex} = this.state;
+        const { currentIndex } = this.state;
 
         console.log("currentIndex = " + currentIndex);
-        console.log("dekc: " + deck);
+        console.log("dekc: " + JSON.stringify(deck));
         return (
-            <View style={{
-                padding: 10
-            }}>
-                <Text>{currentIndex+1}/{deck.questions.length}</Text>
+      
+                
 
-                <Text>{deck.questions.length > 0 && deck.questions[currentIndex].question}</Text>
-            </View>
+                <FlipView style={{ flex: 1 }}
+                    front={this.renderFrontCard(deck, currentIndex)}
+                    back={this.renderBackCard()}
+                    isFlipped={this.state.isFlipped}
+                    onFlipped={(val) => { console.log('Flipped: ' + val); }}
+                    flipAxis="y"
+                    flipEasing={Easing.out(Easing.ease)}
+                    flipDuration={500}
+                    perspective={1000} />
+
+
+
+
+
+            
         )
     }
+    renderFrontCard = (deck, currentIndex) => {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>{currentIndex + 1}/{deck.questions.length}</Text>
+                <Text>{deck.questions.length > 0 && deck.questions[currentIndex].question}</Text>
+                <Text>blalalalal</Text>
+                <TextButton style={{ padding: 10, fontSize: 30 }} onPress={this.flip}>Answer</TextButton>
+
+            </View>
+        );
+    };
+
+    renderBackCard = () => {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <TextButton style={{ padding: 10, fontSize: 30 }} onPress={this.flip}>Queddstion</TextButton>
+
+            </View>
+        );
+    };
 
 }
 
