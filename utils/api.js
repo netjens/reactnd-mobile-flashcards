7502eb9ae838f72bd5d440735c8c7e028d7319d1
netjens <mailtojens@gmail.com>
2017-12-29 17:01:33 +0100

    feat: implementation of required functions

--- utils/api.js
+++ utils/api.js
@@ -2,12 +2,34 @@ import { AsyncStorage } from 'react-native'
 
 const DECK_STORAGE_KEY = 'mobileFlashcards:deck';
 
-export function saveDeckTitle(title){
-    return AsyncStorage.mergeItem(DECK_STORAGE_KEY,JSON.stringify({
-        deckTitle: title, questions : []
-    }))
+export function saveDeckTitle(newTitle) {
+    const newDeck = { title: newTitle, questions: [] }
+    AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({ [newTitle]: newDeck }));
+
 }
 
-export function getDecks(){
-    return AsyncStorage.getItem(DECK_STORAGE_KEY);
+export function getDecks() {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY).then((result) => JSON.parse(result));
+}
+
+export function getDeck(title) {
+    AsyncStorage.getItem(DECK_STORAGE_KEY).then(result => {
+
+        const data = JSON.parse(result);
+        const dataDeck = data[title];
+
+        return dataDeck;
+    });
+
+}
+
+export function saveCardToDeck(title, card) {
+    AsyncStorage.getItem(DECK_STORAGE_KEY).then(result => {
+
+        const data = JSON.parse(result);
+        const dataDeck = data[title];
+        dataDeck.questions = [...dataDeck.questions,card];
+        return dataDeck;
+    }).then(deck => AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({ [title]: deck })));
+
 }
